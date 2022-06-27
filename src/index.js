c00bccd0938629d5ab99f2942d5ebbf2c120a727
Noemi890 <94084046+Noemi890@users.noreply.github.com>
2022-06-27 17:02:23 +0100

    first extension done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 
 // console.log(data);
 const cards = document.querySelector('.cards')
+
+
 data.forEach(element => {
     const card = document.createElement('li')
     card.style.listStyle = 'none'
@@ -15,15 +17,26 @@ data.forEach(element => {
     h2El.innerText = element.name
     imgEl.src = element.sprites.other["official-artwork"].front_default
     imgEl.alt = 'Pokèmon'
-    imgEl.style.width = '256px'
     element.stats.forEach(stat => {
         const liEl = document.createElement('li')
         card.append(liEl)
         liEl.setAttribute('class', 'card--text')
         liEl.innerText = `${stat.stat.name.toUpperCase()}: ${stat.base_stat}`
     })
+    const pEl = document.createElement('p')
+        pEl.setAttribute('class', 'card--text')
+        card.append(pEl)
+        pEl.style.display = 'block'
+        pEl.innerText = 'GAMES APPEREANCE:'
+    element.game_indices.forEach(game => {
+        pEl.style.width = 'auto'
+        const p2El = document.createElement('p')
+        p2El.setAttribute('class', 'card--text')
+        card.append(p2El)
+        p2El.style.display = 'inline-block'
+        p2El.innerText = `${game.version.name}`
+    })
+})
 
-    
 
-});
 // console.log(data[0]);
