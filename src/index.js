64c4adc294247379fcab9b8299c2590e71278f37
Noemi890 <94084046+Noemi890@users.noreply.github.com>
2022-06-27 15:41:42 +0100

    main exercise completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 
-console.log(data);
+// console.log(data);
+const cards = document.querySelector('.cards')
+data.forEach(element => {
+    const card = document.createElement('li')
+    card.style.listStyle = 'none'
+    card.setAttribute('class', 'card')
+    cards.append(card)
+    const h2El = document.createElement('h2')
+    card.append(h2El)
+    h2El.setAttribute('class', 'card--title')
+    const imgEl = document.createElement('img')
+    card.append(imgEl)
+    imgEl.setAttribute('class', 'card--img')
+    h2El.innerText = element.name
+    imgEl.src = element.sprites.front_default
+    imgEl.alt = 'Pokèmon'
+    imgEl.style.width = '256px'
+    element.stats.forEach(stat => {
+        const liEl = document.createElement('li')
+        card.append(liEl)
+        liEl.setAttribute('class', 'card--text')
+        liEl.innerText = `${stat.stat.name.toUpperCase()}: ${stat.base_stat}`
+    })
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+    
+
+});
+// console.log(data[0]);
